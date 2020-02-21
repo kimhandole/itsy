faf2b5eaf800a0a4179902a1caf4c5188d4beccb
kimhandole <hdkimdev@gmail.com>
2020-02-21 01:35:04 -0800

    add navcat link

--- frontend/components/nav_cat/nav_cat.jsx
+++ frontend/components/nav_cat/nav_cat.jsx
@@ -1,31 +1,51 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const NavCat = () => {
-    const category = [
-        "Jewelry & Accessories",
-        "Clothing & Shoes",
-        "Home & Living",
-        "Wedding & Party",
-        "Toys & Entertainment",
-        "Art & Collectibles",
-        "Craft & Supplies",
-        "Vintage",
-        "🎁Gifts"
-    ];
 
-    const categoryItems = [];
-
-    for (const [index, value] of category.entries()) {
-        categoryItems.push(<li className="nav-cat-list-item" key={index}>{value}</li>)
+class NavCat extends React.Component {
+    constructor(props) {
+        super(props);
     }
 
-    return (
-        <div id="nav-cat">
-            <ul className="nav-cat-list">
-                {categoryItems}
-            </ul>
-        </div>
-    );
+    render () {
+        const category = [
+            "Jewelry & Accessories",
+            "Clothing & Shoes",
+            "Home & Living",
+            "Wedding & Party",
+            "Toys & Entertainment",
+            "Art & Collectibles",
+            "Craft & Supplies",
+            "Vintage",
+            "🎁Gifts"
+        ];
+
+        const categoryIds = [
+            1,
+            3,
+            5,
+            7,
+            9,
+            11,
+            13,
+            15,
+            16
+        ];
+
+        const categoryItems = [];
+
+        for (const [index, value] of category.entries()) {
+            categoryItems.push(<Link to={`/categories/${categoryIds[index]}`}><li className="nav-cat-list-item" key={index}>{value}</li></Link>)
+        }
+
+        return (
+            <div id="nav-cat">
+                <ul className="nav-cat-list">
+                    {categoryItems}
+                </ul>
+            </div>
+        );
+    }
 }
 
 export default NavCat;
