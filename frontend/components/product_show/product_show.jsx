0168d4860ad0d044714e8bcf5ba37a11cbbcfe0e
kimhandole <hdkimdev@gmail.com>
2020-02-21 21:12:18 -0800

    add product show fetchshop setState

--- frontend/components/product_show/product_show.jsx
+++ frontend/components/product_show/product_show.jsx
@@ -1,26 +1,33 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
+import LoadingIcon from '../loading/loading_icon';
 
 class ProductShow extends React.Component {
     constructor(props) {
         super(props);
-        this.state({
+        this.state = {
             product: null,
             shop: null
-        });
+        }
     }
 
     componentDidMount() {
         this.props.fetchProduct(this.props.productId)
-        .then(
-            product => {
-                this.props.fetchShop(product.product.product.shop_id)
-            }
-        );
+        .then( product => {
+            this.setState({ product: product });
+            this.props.fetchShop(product.product.product.shop_id)
+            .then( shop => {
+                this.setState({ shop: shop });
+            });
+        });
     }
 
     render() {
-        console.log(this.props);
+        if (this.state.product === null || this.state.shop === null) {
+            return (
+                <LoadingIcon />
+            );
+        }
         return (
             <section className="product-show">
 
