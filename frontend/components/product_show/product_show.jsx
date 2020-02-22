32785e4fc4e584a64251619afbb510c09e1ee67c
kimhandole <hdkimdev@gmail.com>
2020-02-21 21:45:17 -0800

    comment out product show again

--- frontend/components/product_show/product_show.jsx
+++ frontend/components/product_show/product_show.jsx
@@ -5,32 +5,41 @@ import LoadingIcon from '../loading/loading_icon';
 class ProductShow extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            product: null,
-            shop: null
-        }
+        // this.state = {
+        //     product: null,
+        //     shop: null
+        // }
     }
 
     componentDidMount() {
-        this.props.fetchProduct(this.props.productId)
-        .then( product => {
+        // this.props.fetchProduct(this.props.productId)
+        // .then( product => {
+        //     this.setState({ product: product });
+        //     this.props.fetchShop(product.product.product.shop_id)
+        //     .then( shop => {
+        //         this.setState({ shop: shop });
+        //     });
+        // });
+
+        this.props.fetchProduct(this.props.productId).then(product => {
             this.setState({ product: product });
             this.props.fetchShop(product.product.product.shop_id)
-            .then( shop => {
-                this.setState({ shop: shop });
-            });
         });
+
     }
 
     render() {
-        if (this.state.product === null || this.state.shop === null) {
+        if ( !this.props.product || !this.props.shop ) {
             return (
                 <LoadingIcon />
             );
         }
         return (
             <section className="product-show">
+                <img src={this.props.product.photoUrl} alt={this.props.product.title}>
 
+                </img>
+                <p className="product-index-item-title">{this.props.product.title}</p>
             </section>
         );
     }
