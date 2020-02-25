a0b3d58c8c2c925ca8ede3d9098156807192cab0
kimhandole <hdkimdev@gmail.com>
2020-02-25 12:42:29 -0800

    fix seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,24 +7,3 @@ u1 = User.create!(
     first_name: 'Demo User',
     password: '123123'
 )
-
-
-s1 = Shop.create!(
-    title: 'First Shop',
-    owner_id: u1.id
-)
-
-s2 = Shop.create!(
-    title: 'Second Shop',
-    owner_id: u1.id
-)
-
-s3 = Shop.create!(
-    title: 'Beer Shop',
-    owner_id: u1.id
-)
-
-s4 = Shop.create!(
-    title: 'Weed Shop',
-    owner_id: u1.id
-)
