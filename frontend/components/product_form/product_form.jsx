46baf0e0532117654ae71031f1e5238b00a63be7
kimhandole <hdkimdev@gmail.com>
2020-02-25 12:33:50 -0800

    fix price error

--- frontend/components/product_form/product_form.jsx
+++ frontend/components/product_form/product_form.jsx
@@ -104,7 +104,6 @@ class ProductForm extends React.Component {
 
     handleSubmit(e) {
         e.preventDefault();
-        debugger
         const formData = new FormData();
         formData.append('product[title]', this.state.title);
         formData.append('product[product_type]', this.state.productType);
