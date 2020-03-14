fec682ab1a6f72e51e9409e82c2aa051c981c751
kimhandole <hdkimdev@gmail.com>
2020-03-13 17:29:02 -0700

    debug email author

--- frontend/components/session_form/session_form.jsx
+++ frontend/components/session_form/session_form.jsx
@@ -11,7 +11,6 @@ class SessionForm extends React.Component {
     };
 
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleDemoSubmit = this.handleDemoSubmit.bind(this);
   }
 
   update(field) {
