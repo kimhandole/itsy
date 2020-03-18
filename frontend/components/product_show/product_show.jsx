ed5c110f279b98158cad19b7cb33ff4fb8c6a7a9
kimhandole <hdkimdev@gmail.com>
2020-03-18 11:11:40 -0700

    add loading product show

--- frontend/components/product_show/product_show.jsx
+++ frontend/components/product_show/product_show.jsx
@@ -5,26 +5,11 @@ import LoadingIcon from '../loading/loading_icon';
 class ProductShow extends React.Component {
     constructor(props) {
         super(props);
-        // this.state = {
-        //     product: null,
-        //     shop: null
-        // }
+        
     }
 
     componentDidMount() {
-        // this.props.fetchProduct(this.props.productId)
-        // .then( product => {
-        //     this.setState({ product: product });
-        //     this.props.fetchShop(product.product.product.shop_id)
-        //     .then( shop => {
-        //         this.setState({ shop: shop });
-        //     });
-        // });
-
-        // this.props.fetchProduct(this.props.productId).then(product => {
-        //     // this.setState({ product: product });
-        //     this.props.fetchShop(product.product.product.shop_id)
-        // });
+        
     }
 
     price() {
@@ -36,11 +21,11 @@ class ProductShow extends React.Component {
     }
 
     render() {
-        // if (!this.props.shop ) {
-        //     return (
-        //         <LoadingIcon />
-        //     );
-        // }
+        if (!this.props.product ) {
+            return (
+                <LoadingIcon />
+            );
+        }
         return (
             <section className="product-show">
                 <div className="product-show-left">
