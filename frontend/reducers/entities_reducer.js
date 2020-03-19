9e82afd25ff706d9399b1e7f87463b608fd7839a
kimhandole <hdkimdev@gmail.com>
2020-03-19 13:53:30 -0700

    add cart util, action, reducer

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -4,10 +4,13 @@ import users from './users_reducer';
 import shops from './shops_reducer';
 import products from './products_reducer'
 import categories from './categories_reducer';
+import shoppingCartItems from './shopping_cart_item_reducer';
+
 
 export default combineReducers({
     users,
     shops,
     products,
-    categories
+    categories,
+    shoppingCartItems
 });
--- frontend/reducers/errors_reducer.js
+++ frontend/reducers/errors_reducer.js
@@ -1,9 +1,12 @@
 import { combineReducers } from "redux";
 
 import sessionErrorsReducer from "./session_errors_reducer";
+import shoppingCartItem from './shopping_cart_item_errors_reducer';
+
 
 const errorsReducer = combineReducers({
-    session: sessionErrorsReducer
+    session: sessionErrorsReducer,
+    shoppingCartItem
 });
 
 export default errorsReducer;
