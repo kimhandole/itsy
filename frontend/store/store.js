f300a66f8a2ea3ef621060fe15f84e34110d97ac
kimhandole <hdkimdev@gmail.com>
2020-03-23 20:37:12 -0700

    add thunk

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -8,7 +8,7 @@ const configureStore = (preloadedState = {}) => (
     createStore(
         rootReducer,
         preloadedState,
-        applyMiddleware()
+        applyMiddleware(thunk)
     )
 );
 
